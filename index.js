261a1bba6930b753fd9cc5b5128531054204729b
Rithk <rithkchaudhary86@gmail.com>
2024-01-06 16:30:09 +0530

    user route

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin.js")
-//const userRouter = require("./routes/user");
+const userRouter = require("./routes/user");
 
 app.use(bodyParser.json());
 app.use("/admin", adminRouter)
-//app.use("/user", userRouter)
+app.use("/user", userRouter)
 
 const PORT = 3001;
 app.listen(PORT, () => {
